5533b24afa54edfc8371464b5673d62bf4443d67
Sanjeeb.Guru <guru.sanjeeb@gmail.com>
2019-07-22 20:35:19 +0530

    itemtouch listener added

--- app/src/main/java/com/gurus/sciencereads/MainActivity.java
+++ app/src/main/java/com/gurus/sciencereads/MainActivity.java
@@ -3,10 +3,13 @@ package com.gurus.sciencereads;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 
 import com.gurus.sciencereads.adapter.NewsRecyclerViewAdapter;
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding activityMainBinding;
     private NewsViewModel newsViewModel;
     private NewsRecyclerViewAdapter adapter;
+    ItemTouchHelper.SimpleCallback simpleCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +40,24 @@ public class MainActivity extends AppCompatActivity {
         newsViewModel.init(this);
         newsViewModel.getNewsData().observe(this, new Observer<List<Article>>() {
             @Override
-            public void onChanged(@Nullable List<Article> newsItemModels) {
+            public void onChanged(@Nullable final List<Article> newsItemModels) {
                 Log.d(TAG, "changed"+newsItemModels.size());
                 adapter = new NewsRecyclerViewAdapter(MainActivity.this, newsItemModels);
                 activityMainBinding.newsList.setAdapter(adapter);
+                simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+                    @Override
+                    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+                        return false;
+                    }
+
+                    @Override
+                    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                        newsItemModels.remove(viewHolder.getAdapterPosition());
+                        adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+                    }
+                };
+                ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+                itemTouchHelper.attachToRecyclerView(activityMainBinding.newsList);
             }
         });
        // Controller controller = new Controller();
